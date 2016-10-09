115ebd068931517f90aa91724945e6cb042379c3
matig123 <matiasgrotewold11@gmail.com>
2016-10-09 16:56:58 -0400

    some psuedo code and methods we will need to write

---
+++ Assets/FirstAgent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstAgent : MonoBehaviour {
+
+	float visionLimit = 10;
+	Vector3 agentPosition, agentOrientation, objectPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void TurnLeft(){
+	}
+
+	void TurnRight(){
+	}
+
+	void MoveForward(){
+	}
+
+	void MoveBackward(){
+	}
+
+	void FieldOfView(){
+		//what we will need to use somewhere:
+		Vector3 agentToVertex = objectPosition - agentPosition;
+		agentToVertex.Normalize();
+		if(Vector3.Dot(agentToVertex, agentOrientation) > visionLimit){
+			//object is in vision
+		}
+	}
+}
