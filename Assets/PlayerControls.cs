ba2247e79f6e6d02288c35fdc5d7114e946537aa
Matt Cramblett <Cramblett.10@osu.edu>
2016-10-10 15:20:27 -0400

    player controls and color. movement is intentionally one direction at a time. This should help with implementation later

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float rate = 5f;
+	
+	// Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * rate * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * rate * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.W)){
+        	transform.position += Vector3.forward * rate * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.S)){
+			transform.position -= Vector3.forward * rate * Time.deltaTime;
+        }
+    }
+}
