e969da675a367f4508ee948e26c4d6f1806dd501
matig123 <matiasgrotewold11@gmail.com>
2016-10-13 08:10:35 -0400

    logic for what will happen if visible, when agent loses interest etc. Untested because still need to write vision test and related move stuf

--- Assets/Predator.cs
+++ Assets/Predator.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class Predator : MonoBehaviour {
 
 	public float rate = 5f;
-	PredatorSprite p1;
+	int numPredators = 1;
+	public ArrayList Predators;
+	int attackThreshold = 5;
+	int loseInterestThreshold = 5;
 
 	public class PredatorSprite {
 		public GameObject body;
@@ -13,6 +16,7 @@ public class Predator : MonoBehaviour {
 		bool alerted; //sees Prey from a distance, starts following slowly
 		bool attacking; //sees Prey close by, follows prey quickly
 		bool swarming; //alerts other predators - they switch to attack mode
+		int loseInterestCount;
 
 
 		//CONSTRUCTOR:
@@ -23,6 +27,7 @@ public class Predator : MonoBehaviour {
 			alerted = false;
 			attacking = false;
 			swarming = false;
+			loseInterestCount = 0;
 			//semi-random start position: (currently top corner area)
 			body.transform.position = new Vector3(Random.Range(5f, 15f), 0.5f, Random.Range(5f, 15f));
 			//Add the material for color:
@@ -31,6 +36,29 @@ public class Predator : MonoBehaviour {
 				body.GetComponent<Renderer>().material = material;
 			//set name of PredatorSprite object (may come in handy for lookup):
 			body.name = name;
+		}
+		//accessors
+		public bool getAttack(){
+			return attacking;
+		}
+		public bool getRoaming(){
+			return roaming;
+		}
+		public bool getAlerted(){
+			return alerted;
+		}
+		public bool getSwarming(){
+			return swarming;
+		}
+
+		public int interest(string todo){
+			if (todo.Equals ("increase")) {
+				loseInterestCount++;
+			}
+			if (todo.Equals ("reset")) {
+				loseInterestCount = 0;
+			}
+			return loseInterestCount;
 		}
 
 		/* this method could return an update to a position vector, but otherwise should
@@ -61,23 +89,52 @@ public class Predator : MonoBehaviour {
 
 		}
 
+		public int visionTest(){
+			int distance = -1;
+			return distance;
+		}
+
 	}
 
-	// Use this for initialization
+	// Use this for initialization. initialize several, add to array
 	void Start () {
-		p1 = new PredatorSprite("predator1");
+		generatePredators ();
 	}
-	
+
+	void generatePredators(){
+		Predators = new ArrayList ();
+		for (int i = 0; i < numPredators; i++) {
+			PredatorSprite predator = new PredatorSprite ("predator" + i.ToString ());
+			predator.body.transform.parent = transform;
+			predator.body.name = "predator" + i.ToString ();
+			Predators.Add (predator);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.A)){
-            p1.body.transform.position += Vector3.left * rate * Time.deltaTime;
-        }else if (Input.GetKey(KeyCode.D)){
-            p1.body.transform.position += Vector3.right * rate * Time.deltaTime;
-        }else if (Input.GetKey(KeyCode.W)){
-        	p1.body.transform.position += Vector3.forward * rate * Time.deltaTime;
-        }else if (Input.GetKey(KeyCode.S)){
-			p1.body.transform.position -= Vector3.forward * rate * Time.deltaTime;
-        }
+		foreach (PredatorSprite p in Predators){
+			int preySeen = p.visionTest ();
+			//may need to change this: if value is so low, probably touched and game over
+			if (preySeen >= 0) {
+				//prey is within attack range and has not lost interest
+				if (preySeen >= attackThreshold && p.interest() < loseInterestThreshold) {
+					//if attacking, make sure lose interest count remains at 0
+					p.interest ("reset");
+					p.attack ();
+				} else {
+					//if attacking or alerted but not seeing prey, increase lose interest count
+					if (p.getAttack () || p.getAlerted ()) {
+						p.interest ("increase");
+					}
+					//alerted does not affect interest - aka easy to lose when alerted
+					p.alert ();
+				}
+			} else {
+				//if has lost sight, loss of interest increases by 1
+				p.interest ("increase");
+				p.roam ();
+			}
+		}
 	}
 }
